757da799e447c115109579b507c00913bf3111a5
Klecia Vladmaker <klecia.vladmaker@hotmail.com>
2021-01-25 20:34:14 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,7 +31,11 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-
+		
+		System.out.println("\n==== TEST 4: seller insert =====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
